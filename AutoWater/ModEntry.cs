f9ac552e1a0b711a023f50217aaeba00121d3ff4
Steph Hoel <steph.hoel@gmail.com>
2024-04-10 13:58:11 -0300

    refactor: muda uso de lista de locais para locais disponíveis no jogo

--- AutoWater/ModEntry.cs
+++ AutoWater/ModEntry.cs
@@ -1,4 +1,6 @@
 ﻿using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Buildings;
 using StardewValley.Objects;
 using StardewValley.TerrainFeatures;
 
@@ -15,34 +17,39 @@ public class ModEntry : Mod
 
     private async void WaterEverything(object sender, EventArgs e)
     {
-        foreach (var location in Utils.Locations.GetLocations())
+        foreach (var location in Game1.locations)
         {
-            //Monitor.Log($"{location} is {(locationGame is null ? "null" : "not null")}", LogLevel.Debug);
+            //Monitor.Log($"==========> {location.Name} is {(location is null ? "null" : "not null")}", LogLevel.Debug);
 
-            if (location is not null)
+            // Garden Pots
+            var objects = location.objects.Values.OfType<IndoorPot>();
+            foreach (var pot in objects)
             {
-                // Garden Pots
-                var objects = location.objects.Values.OfType<IndoorPot>();
-                foreach (var pot in objects)
-                {
-                    //Monitor.Log($"{location} with Garden Pot to add water", LogLevel.Debug);
-                    pot.Water();
-                }
+                //Monitor.Log($"{location.Name} with Garden Pot to add water", LogLevel.Debug);
+                pot.Water();
+            }
 
-                // Hoe Dirts
-                var terrains = location.terrainFeatures.Values.OfType<HoeDirt>();
-                foreach (var terrain in terrains)
-                {
-                    //Monitor.Log($"{location} with HoeDirt to add water", LogLevel.Debug);
-                    terrain.state.Value = 1;
-                }
+            // Hoe Dirts
+            var terrains = location.terrainFeatures.Values.OfType<HoeDirt>();
+            foreach (var terrain in terrains)
+            {
+                //Monitor.Log($"{location.Name} with HoeDirt to add water", LogLevel.Debug);
+                terrain.state.Value = 1;
             }
         }
 
-        Building farm = Game1.getFarm().getBuildingByType("Pet Bowl");
-        PetBowl pet = (PetBowl)farm;
-        pet.watered.Value = true;
-        // Monitor.Log("PetBowl is full", LogLevel.Debug);
+        // Water to pet bowl
+        try
+        {
+            var farmPetBowl = Game1.getFarm().getBuildingByType("Pet Bowl");
+            PetBowl pet = (PetBowl)farmPetBowl;
+            pet.watered.Value = true;
+            // Monitor.Log("PetBowl is full", LogLevel.Debug);
+        }
+        catch
+        { // For eventually error
+            Monitor.Log("PetBowl do not exist", LogLevel.Debug);
+        }
     }
 
     private void RemoveObsoleteFiles(IModHelper helper, string[] files)
