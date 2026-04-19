8b92d2eebce2870746307892820e07f2ee98d78f
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2026-04-19 09:30:32 -0300

    refactor: simplify Object checkForAction patch logic

--- ConfigureMachineSpeed/Patches/MachineCollectPatch.cs
+++ ConfigureMachineSpeed/Patches/MachineCollectPatch.cs
@@ -1,17 +1,28 @@
 using HarmonyLib;
+using StardewValley.Extensions;
+using Object = StardewValley.Object;
 
 namespace StephHoel.ConfigureMachineSpeed.Patches;
 
-[HarmonyPatch(typeof(StardewValley.Object), nameof(StardewValley.Object.checkForAction))]
+[HarmonyPatch(typeof(Object), nameof(Object.checkForAction))]
 public static class MachineCollectPatch
 {
-    public static void Postfix(StardewValley.Object __instance, bool __result)
+    public static void Postfix(Object __instance, bool __result)
     {
-        if (__result
-            || !__instance.bigCraftable.Value
-            || __instance.heldObject.Value == null)
+        if (!IsDropIn(__instance, __result))
             return;
 
-        __instance.ConfigureOneMachine(ModEntry.Instance.Helper.ReadConfig<ModConfig>());
+        if (__instance.IsCrystalarium())
+            __instance.ConfigureOneMachine(ModEntry.Instance.Helper.ReadConfig<ModConfig>());
+    }
+
+    private static bool IsDropIn(Object __instance, bool __result)
+    {
+        return !__result || __instance.bigCraftable.Value || __instance.heldObject.Value is not null;
+    }
+
+    private static bool IsCrystalarium(this Object machine)
+    {
+        return machine.BaseName.EqualsIgnoreCase("Crystalarium");
     }
 }
